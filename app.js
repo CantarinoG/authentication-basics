89d63d3cad5594427d1f708098ccdc76bd97fca4
CantarinoG <guilhermecantarino1709@gmail.com>
2023-02-21 20:12:47 -0300

    Add index view and fix app.js typos

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 const mongoDb = process.env.MONGODB_URI;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 //Check if error on connection
-const db = mongoose.connections;
+const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
 
 //Creating data schema
